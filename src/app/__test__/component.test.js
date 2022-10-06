edb152a204a309c7dfe8b794b9e069bd4ba8d555
Federico Luna <fluna1973@yahoo.com>
2022-10-05 20:07:49 -0600

    setup tests

---
+++ src/app/__test__/component.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Circuits from '../../Modules/Circuits/Circuits';
+import Drivers from '../../Modules/Drivers/Drivers';
+import DriverStandings from '../../Modules/DriverStandings/DriverStandings';
+import { store } from '../../Redux/store';
+
+describe('Rocket components', () => {
+  it('Render component', () => {
+    const tree = render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            <Circuits />
+          </Router>
+        </Provider>
+      </React.StrictMode>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Mission components', () => {
+  it('Render component', () => {
+    const treeMission = render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            <Drivers />
+          </Router>
+        </Provider>
+      </React.StrictMode>,
+    );
+    expect(treeMission).toMatchSnapshot();
+  });
+});
+
+describe('Testing the Profile component', () => {
+  test('Check rendering of the Profile component', () => {
+    const { getByText } = render(
+      <Router>
+        <Provider store={store}>
+          <DriverStandings />
+        </Provider>
+      </Router>,
+    );
+    expect(getByText('Pilot')).toBeInTheDocument();
+  });
+});
