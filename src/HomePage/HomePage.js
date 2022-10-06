aa206533f70a20439ebd238e34bc213e45814ca7
Federico Luna <fluna1973@yahoo.com>
2022-10-06 09:53:39 -0600

    set hover over cards

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -5,18 +5,22 @@ const Home = () => (
   <>
     <div className="pictureContainer">
       <Link to="/Drivers">
-        <img src="images/Cars/Helmets.jpeg" alt="hformula 1 hemlets" className="drLinkH" />
-        <h1 className="driverTitle">Drivers</h1>
+        <div className="topImage">
+          <img src="images/Cars/Helmets.jpeg" alt="hformula 1 hemlets" className="drLinkH" />
+          <h1 className="driverTitle">Drivers</h1>
+        </div>
       </Link>
       <div className="home2">
         <Link style={{ textDecoration: 'none' }} to="/DriverStandings">
           <img src="images/Cars/stats.png" alt="hformula 1 hemlets" className="otherLinkH" />
           <h1 className="standTitle">Standings</h1>
         </Link>
-        <Link style={{ textDecoration: 'none' }} to="/Circuits">
-          <img src="images/Cars/imola.jpeg" alt="hformula 1 hemlets" className="otherLinkH" />
-          <h1 className="circuitTitle">Circuits</h1>
-        </Link>
+        <div className="home3">
+          <Link style={{ textDecoration: 'none' }} to="/Circuits">
+            <img src="images/Cars/imola.jpeg" alt="hformula 1 hemlets" className="otherLinkH" />
+            <h1 className="circuitTitle">Circuits</h1>
+          </Link>
+        </div>
       </div>
     </div>
     <div className="wapper">
